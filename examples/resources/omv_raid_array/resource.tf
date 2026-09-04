944e2214d1c1a6d04182a484c079bf1e194919fd
Sib <sibalot@hotmail.fr>
2026-09-04 11:48:41 +0200

    add example

---
+++ examples/resources/omv_raid_array/resource.tf
@@ -0,0 +1,6 @@
+resource "openmediavault_raid_array" "example" {
+  name        = "my_raid_array"
+  devices     = ["disk1", "disk2", "disk3"]
+  raid_level  = "raid5"
+  description = "A test RAID array for OMV"
+}
